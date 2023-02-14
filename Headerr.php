4561d4c5658af3d05eb2051838d073b26dc245cd
Kushani Gnanthilaka <116035588+Kushi9572@users.noreply.github.com>
2023-02-14 13:18:56 +0200

    index,footer and header

---
+++ Headerr.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>echo $title</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <style>
+body {
+    background-color:#EF92D5;
+    font-family:'Courier New', Courier, monospace;
+    color:#680a11;
+    font-size: 13.5px;
+    width: 1080px;
+    margin:auto;
+    display: flex;
+    justify-content: space-between;
+}
+h3{ 
+    color:#680a11;
+    font-size: 12px;
+    padding-top: 325px;
+    padding-bottom:20px ;
+    padding-left: 150px;
+}
+
+img{
+    margin-right: 50px;
+    margin-left:5px;
+    margin-bottom: 0px;
+}
+nav li {
+    font-size: 20px;
+    display:inline-block;
+    margin-right:60.5px;
+    margin-left: 10px;
+    margin-bottom:0px;
+}
+nav li:last-child {
+    margin-right: 0px;
+}
+nav ul {
+    padding-left: 0;    
+}
+.nav-item {
+    font-size: 20px;
+    text-align: center;
+
+}
+h1{
+    font-size: 20px;
+    text-align: justify;
+}
+.header-image {
+    width: 100%;
+    height: auto;
+}
+h2{
+    background-color:#EF92D5;
+    height: 0px;
+    padding-top: 20px;
+    padding-bottom:20px ;
+    padding-left: 20px;
+    text-align: center;
+    font-size: 22px;
+    font-size: 18px;
+    
+}
+/*footer*/
+
+footer{
+    margin-top:0px;
+    background-color:#FD0DBA;
+    margin-left: 9px;
+    margin-bottom: 15px;
+}
+
+p{
+
+    height: 0px;
+    padding-top: 20px;
+    padding-bottom:30px ;
+    padding-left: 20px;
+    text-align: center;
+    font-size: 15px;
+    text-align: justify;
+    font-weight: bold;
+
+}
+
+.img-fluid {
+    width:fit-content;
+}
+
+@media  (max-width: 450px) {
+
+    body {
+        width: fit-content;
+        margin: 0 16px 16px 16px;
+            
+    }
+    
+    .wrapper {
+        display : flex ;
+        justify-content : center ;
+        align-items:center ;
+        height : auto;
+        width: 100%;
+    }
+    
+    img {
+        display: block;
+        margin: 0 0 16px 0;
+    
+    }
+}
+
+    </style>
+</head>
+<body>
+  <div class="container">
+    <header class="site_header">
+        <div class="wrapper"></div>
+        <nav>
+
+            <ul>
+              <ul>
+                <li><a href="#"><i><b>Twinkling Stars<br>Kindergarten</a></i></b></li>
+                <img src="logo.png" class="rounded" alt="Logo" width="100" height="75">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="activitiesneda.html">Activities</a></li>
+                <li><a href="contactasiri.html">Contact</a></li>
+                <a class="btn btn-danger" href="enrollsonali.html" role="button">Enroll Today</a>
+              </ul>
+                   
+            </ul>
+                </nav>
+
+</header>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+   
+    <div class="card text-bg-dark">
+            <img src="home.png" class="card-img" alt="image">
+            <div class="card-img-overlay">
+          <h3 class="card-text"><b>When you <br> enter this <br> Loving School <br>Consider yourself <br> One of the special <br>
+            Members of an <br> Extraordinary Family</b></h3>
+    </div>
+</div>
---
+++ Index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<?php include "Headerr.php" ?>
+    <h2><b>We are group of teachers  who really  love children's and enjoy every moment of teaching and playing with 
+        our students.</b></h2>
+
+<h2><b>link for ex1.php </b></h2>
+<li class="nav-item">
+          <a class="nav-link" href="Exercise 1.php">ex1.php</a>
+        </li>
+
+
+<?php include "Footer1.php" ?>
+
+
+
