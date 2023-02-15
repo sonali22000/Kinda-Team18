42f153425f263b872e6b8a674a5213b1dd8997f2
Kushani Gnanthilaka <116035588+Kushi9572@users.noreply.github.com>
2023-02-15 09:53:53 +0200

    changed

--- Headerr.php
+++ Headerr.php
@@ -141,12 +141,4 @@ p{
 
 </header>
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-   
-    <div class="card text-bg-dark">
-            <img src="home.png" class="card-img" alt="image">
-            <div class="card-img-overlay">
-          <h3 class="card-text"><b>When you <br> enter this <br> Loving School <br>Consider yourself <br> One of the special <br>
-            Members of an <br> Extraordinary Family</b></h3>
-    </div>
-</div>
+  
