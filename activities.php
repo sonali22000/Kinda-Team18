a5c935639da193385fae0215e022117bcd9cf038
Kushani Gnanthilaka <116035588+Kushi9572@users.noreply.github.com>
2023-02-15 18:38:08 +0200

    new

--- activities.php
+++ activities.php
@@ -15,7 +15,7 @@
             
                 <div class="col-md-4 text-center">
                   <div class="card" style="wwidth: 18rem;">
-                    <img src="https://s3-alpha-sig.figma.com/img/02ba/f36e/37b26b09591c7f43bcb132076fd8149c?Expires=1675641600&Signature=njXsIouMbbmqAV0vtGXo-FxI4LuyyBPh9vFiypc649ML743cu~1FmVCa47AbQM3QqEZGDo2SPE7dsaMftiBwdOKaoof9tid7ac8rJ8HrgeGfRQx8NHjWti4GTFqkZantO5T9yAgGjO3Fh97UAC~jhROBcMwSO~mP2WFEhJIB59FS2PD2mW8FjjUOBv6-ITrkvZhjVsUyOiLtLfJqQ-Db-v~Og2TVSNf~hMvPVEvzAKM9eVRuw3n-0aLRwqaF0v1yBBSiHOGbGX8sCyr8mEt~83WH~wMwiOstHBEr9MP8g1g22GNPgVQzlkZp0Nr7vdBedtpiDAbCm433Sb~DSFgELQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" class="card-img-top" alt="SPORTS ACTIVITIES">
+                    <img src="activities2.png" class="card-img-top" alt="SPORTS ACTIVITIES">
                     <div class="card-body">
                       <p class="card-text"> <h4>SPORTS ACTIVITIES</h4> With an aim to build flexibility with teamspirit ans confidence amongst childrens, many sports activities such as yoga, taekwondo are organaised</p>
                     </div>
