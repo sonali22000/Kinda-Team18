21cf72a9902e884e72443b55c18f2a2b7a82949e
udayanga22000 <118618291+udayanga22000@users.noreply.github.com>
2023-02-17 10:05:10 +0200

    Adding foam

--- contact.php
+++ contact.php
@@ -31,4 +31,42 @@
           </div>
         
 
+<div class="row">
+    <div class="col-md-6" style="color: aliceblue; text-align: center; font-size:35px; color: cornflowerblue; font-weight: bold; font-style: italic;  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;">
+         
+      <p1>Get in touch<br>   
+        <br>     
+    </p1>
+    </div>
+    <div class="col-md-6">         
+        </div>
+    </div>
+
+<div class="row">
+    <div class="col-md-6" style="color: aliceblue; text-align: center; font-size: larger;">
+ 
+    </div>
+    <div class="col-md-6" style="color: aliceblue;"> 
+
+        <form action="/action_page.php">
+            <label for="name" >Name*      : </label>
+            <div><input type="text" id="name" name="name" size="45"><br><br></div>
+            <label for="email">Email *    : </label>
+            <div><input type="text" id="email" name="email" size="45"><br><br></div>
+            <label for="subject">Subject : </label>
+            <div><input type="text" id="subject" name="subject" size="45"><br><br></div>
+            <label for="lname">Message* : </label>
+            <div><input type="text" id="msg" name="msg" size="45"><br><br></div>
+            <label for="lname">Phone *: </label>
+            <div><input type="text" id="phone" name="phone" size="45"><br><br></div>
+
+            <input type="submit" value="Submit" style="color:coral; font-weight: bold; background-color: blanchedalmond;">
+          </form>
+          
+        
+        </div>
+    </div>
+
+                       
+
 <?php include "footer1.php" ?>
