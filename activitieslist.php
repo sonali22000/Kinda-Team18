d2930a5d65922ffdaf93fe360a7d49beb0022b34
nedaMaghrebi <neda22000@student.hamk.fi>
2023-02-18 14:27:48 +0200

    the collum adde to activitiestable.php

---
+++ activitieslist.php
@@ -0,0 +1,61 @@
+<?php
+$title="crup app for kindergarten";
+include "headerr.php"?>
+
+
+<!-- creating form for kidsinfo-->
+
+<form  method="post"  action=""> 
+ <div class="row">
+    <div class="col">
+    <input type="text" name="aname" required placeholder="Activities Name" class="form-control" ></div>
+    
+    <div class="col">
+    <input type="text" name="description" required placeholder="Description" class="form-control"></div>
+
+    <div class="col">
+    <input type="text" name="location" required placeholder="Location" class="form-control"> <br></div>
+ </div> 
+
+
+ <div class="row">
+   <div class="col">
+   <input type="text" name="startdate" required placeholder="Start Date" class="form-control" > </div>
+
+   <div class="col">
+   <input type="text" name="enddate" required placeholder="End Date" class="form-control" > </div>
+
+   <div class="col">
+   <input type="text" name="capacity" required placeholder="Capacity" class="form-control" ><br> </div>
+ </div>
+
+
+ <div class="row"> 
+   <div class="col">
+   <input type="submit" value="Submit" name="Submit"></div>
+ </div>
+</form>
+
+
+<?php
+//Getting values from end user
+if(isset($_POST['submit'])){
+    $fname=$_POST['fname'];
+    $lname=$_POST['lname'];
+    $parentsname=$_POST['parentsname'];
+    $dob=$_POST['dob'];
+    $phone=$_POST['phone'];
+    $address=$_POST['address'];
+    include 'db.php';
+    $sql="insert into kidsinfo(fname,lname,parentsname,dob,phone,address)
+    values('$fname','$lname','$parentsname', '$dob', '$phone', '$address')";
+    if($conn->query($sql)===TRUE){
+        echo"Your information is added successfully";
+    }
+    else{
+        echo"Erro: .$conn->error";
+    }
+}
+?>
+
+<?php include "footer1.php" ?>
