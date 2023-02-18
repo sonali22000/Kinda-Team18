eced3153f5f994e36337288dce7c2606fff1d7e2
nedaMaghrebi <neda22000@student.hamk.fi>
2023-02-18 12:21:39 +0200

    collum for kids info added

--- crudapp/create.php
+++ crudapp/create.php
@@ -5,34 +5,35 @@ include "headerr.php" ?>
 
 <!-- creating form for kidsinfo-->
 
-    <form  method="post"  action="">
-    
-<div class="row">
-
+<form  method="post"  action=""> 
+ <div class="row">
     <div class="col">
     <input type="text" name="fname" required placeholder="Kids First Name" class="form-control" > </div>
     
     <div class="col">
-    <input type="text" name="lname" required placeholder="Last Name" class="form-control"></div>
+    <input type="text" name="lname" required placeholder="Kids Last Name" class="form-control"></div>
 
-</div>
+    <div class="col">
+    <input type="text" name="parentsname" required placeholder="Parents Name" class="form-control"></div>
+ </div> 
 
 
-<div class="row">
+ <div class="row">
+   <div class="col">
+   <input type="text" name="dob" required placeholder="Birth Date" class="form-control" > </div>
 
    <div class="col">
-   <input type="text" name="bdate" required placeholder="Birth Date" class="form-control" > </div>
-    
+   <input type="text" name="phone" required placeholder="Phone Number" class="form-control" > </div>
+
    <div class="col">
-   <input type="text" name="color" required placeholder="Select fav Color" class="form-control" > </div>
-
-   <input type="submit" value="Submit">
-
-</div>
+   <input type="text" name="address" required placeholder="Address" class="form-control" > </div>
+ </div>
 
 
-
-
+ <div class="row"> 
+   <div class="col">
+   <input type="submit" value="Submit" name="Submit"></div>
+ </div>
 </form>
 
 
