7dee1db3eb2db36bacb262c78340055ccca9275b
Kushani Gnanthilaka <116035588+Kushi9572@users.noreply.github.com>
2023-02-20 01:06:44 +0200

    ceating application

---
+++ createindex.php
@@ -0,0 +1,29 @@
+<?php
+$title="crup app for kindergarten";
+include "headerr.php"?>
+
+
+<!-- creating form for applying teacher-->
+
+<form  method="post"  action=""> 
+ <div class="row">
+    <div class="col">
+    <input type="text" name="aname" required placeholder="Name" class="form-control" ><br></div>
+ </div>    
+
+ <div class="row">
+    <div class="col">
+    <input type="text" name="phoneno" required placeholder="Contact number" class="form-control"><br></div>
+ </div>   
+
+ <div class="row">
+    <div class="col">
+    <input type="text" name="email" required placeholder="email" class="form-control"><br></div>
+ </div> 
+
+</form>
+
+
+
+
+<?php include "footer1.php" ?>
