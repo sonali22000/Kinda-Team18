b4ec0e974ed7983e4c6d7f69af14db05c5aab35e
nedaMaghrebi <neda22000@student.hamk.fi>
2023-02-20 19:56:33 +0200

    make some changes in activities.php

--- activities.php
+++ activities.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php include "headerr.php" ?>
 
-                     
+            <!-- creating different activities in kindergarten-->               
                 <div class="row">
                     <div class="col-md-4 text-center">
             
@@ -27,15 +27,92 @@
                   <div class="card" style="wwidth: 18rem;">
                     <img src="image/activities3.jpg" class="card-img-top" alt="EXCURSIONSS">
                     <div class="card-body">
-                      <p class="card-text" > <h4>EXCURSIONS</h4> Children go out and explore places such as museum, zoo or go for picnic in a fun way. These excurstions are packed with many interesting snacks.</p>
+                      <p class="card-text" > <h4>EXCURSIONS</h4> Children go out and explore places such as museum, zoo or they go for picnic in a fun way. These excurstions also are packed with many interesting snacks.</p>
                     </div>
                   </div>
                 </div>
             </div>
+<br>
+
+
+
+ <!-- creating form for program in kindergarten-->
+
+<form  method="post"  action=""> 
+
+
+<div class="row">
+   <div class="col-md-3">
+   <input type="text" name="pname" required placeholder="Activities Name" class="form-control" ></div>
+   
+   <div class="col-md-3">
+   <input type="text" name="description" required placeholder="Description" class="form-control"> <br></div>
+
+
+<div class="row">
+  <div class="col-md-3">
+  <input type="date" name="startdate" required placeholder="Please Enter Activity Start Date" class="form-control" > </div>
+
+  <div class="col-md-3">
+  <input type="date" name="enddate" required placeholder="Please Enter Activity End Date" class="form-control" > <br> </div>
+</div>
+
+<div class="row">
+  <div class="col-md-3">
+  <input type="int" name="capacity" required placeholder="Capacity" class="form-control" >  <br> </div>
+</div>
+
+
+<div class="row">
+  <div class="col-md-3">
+  <input type="int" name="kidsid" required placeholder="Kidsid" class="form-control" > <br> </div>
+</div>
+
+<div class="row">
+  <div class="col-md-3">
+  <input type="int" name="teid" required placeholder="Teid" class="form-control" >  <br> </div>
+</div>
+
+
+
+<div class="row"> 
+  <div class="col-md-3">
+  <input type="submit" value="Submit" name="Submit"> <br> </div>
+</div>
+</form>
+
+
+<?php
+//Getting values from end user
+if(isset($_POST['Submit'])){
+   $aname=$_POST['pname'];
+   $description=$_POST['description'];
+   $startdate=$_POST['startdate'];
+   $enddate=$_POST['enddate'];
+   $capacity=$_POST['capacity'];
+   $kidsid=$_POST['kidsid'];
+   $teid=$_POST['teid'];
+   include 'db.php';
+   $sql="insert into activitieslist(pname,description,startdate,enddate,capacity,kidsid,teid)
+   values('$aname','$description', '$startdate', '$enddate' , '$kidsid', '$teid')";
+   if($conn->query($sql)===TRUE){
+               
+     echo"Your datas are added successfully";
+
+ }
+ else{
+
+     echo"Error:" .$conn->error;
+
+ }
+}
+?>
         
                
 </body> 
 </html>
+
+<br>
 <?php include "footer1.php" ?>
 
 
--- createprogram.php
+++ createprogram.php
@@ -3,36 +3,47 @@ $title="crup app for kindergarten";
 include "headerr.php"?>
 
 
-<!-- creating form for kidsinfo-->
+<!-- creating form for program in kindergarten-->
 
 <form  method="post"  action=""> 
+
+
  <div class="row">
-    <div class="col">
-    <input type="text" name="aname" required placeholder="Activities Name" class="form-control" ></div>
+    <div class="col-md-3">
+    <input type="text" name="pname" required placeholder="Activities Name" class="form-control" ></div>
     
-    <div class="col">
-    <input type="text" name="description" required placeholder="Description" class="form-control"></div>
-
-    <div class="col">
-    <input type="text" name="location" required placeholder="Location" class="form-control"> <br></div>
- </div> 
+    <div class="col-md-3">
+    <input type="text" name="description" required placeholder="Description" class="form-control"> <br></div>
 
 
  <div class="row">
-   <div class="col">
+   <div class="col-md-3">
    <input type="date" name="startdate" required placeholder="Please Enter Activity Start Date" class="form-control" > </div>
 
-   <div class="col">
-   <input type="date" name="enddate" required placeholder="Please Enter Activity End Date" class="form-control" > </div>
+   <div class="col-md-3">
+   <input type="date" name="enddate" required placeholder="Please Enter Activity End Date" class="form-control" > <br> </div>
+ </div>
 
-   <div class="col">
-   <input type="int" name="capacity" required placeholder="Capacity" class="form-control" ><br> </div>
+ <div class="row">
+   <div class="col-md-3">
+   <input type="int" name="capacity" required placeholder="Capacity" class="form-control" >  <br> </div>
+ </div>
+
+
+ <div class="row">
+   <div class="col-md-3">
+   <input type="int" name="kidsid" required placeholder="Kidsid" class="form-control" > <br> </div>
+ </div>
+
+ <div class="row">
+   <div class="col-md-3">
+   <input type="int" name="teid" required placeholder="Teid" class="form-control" >  <br> </div>
  </div>
  
 
 
  <div class="row"> 
-   <div class="col">
+   <div class="col-md-3">
    <input type="submit" value="Submit" name="Submit"></div>
  </div>
 </form>
@@ -41,15 +52,16 @@ include "headerr.php"?>
 <?php
 //Getting values from end user
 if(isset($_POST['Submit'])){
-    $aname=$_POST['aname'];
+    $aname=$_POST['pname'];
     $description=$_POST['description'];
-    $location=$_POST['location'];
     $startdate=$_POST['startdate'];
     $enddate=$_POST['enddate'];
     $capacity=$_POST['capacity'];
+    $kidsid=$_POST['kidsid'];
+    $teid=$_POST['teid'];
     include 'db.php';
-    $sql="insert into activitieslist(aname,description,location,startdate,enddate,capacity)
-    values('$aname','$description','$location', '$startdate', '$enddate', '$capacity')";
+    $sql="insert into activitieslist(pname,description,startdate,enddate,capacity,kidsid,teid)
+    values('$aname','$description', '$startdate', '$enddate' , '$kidsid', '$teid')";
     if($conn->query($sql)===TRUE){
                 
       echo"Your datas are added successfully";
