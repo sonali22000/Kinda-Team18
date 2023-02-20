bf2d2108017d867c9d8c06e04a49869a9d92fa67
Kushani Gnanthilaka <116035588+Kushi9572@users.noreply.github.com>
2023-02-20 02:01:14 +0200

    add informations

--- createindex.php
+++ createindex.php
@@ -6,20 +6,67 @@ include "headerr.php"?>
 <!-- creating form for applying teacher-->
 
 <form  method="post"  action=""> 
+
  <div class="row">
-    <div class="col">
-    <input type="text" name="aname" required placeholder="Name" class="form-control" ><br></div>
+    <div class="col-md-6">
+    <h4><b>Applying for the post of Kindergarden Teacher</b></h4>
+    </div>
+    <div class="col-md-6" style="text-align:left;">
+    <input type="text" name="aname" required placeholder="Name" class="form-control" ><br>
+    </div>
  </div>    
 
  <div class="row">
-    <div class="col">
-    <input type="text" name="phoneno" required placeholder="Contact number" class="form-control"><br></div>
+ <div class="col-md-6">
+    <h5>Description</h5>
+    <p>We are looking for an enthusiastic kindergarten teacher to foster and facilitate the intellectual 
+        and social development of the children. Teaching in kindergarten includes planning, implementing 
+        and assessing lessons. You will teach children with patience and creativity. Kindergarten Teacher
+        duties include designing a teaching plan and using activities and instructional methods to motivate
+        children.</p>
+    </div>
+    <div class="col-md-6" style="text-align:left;">
+    <input type="text" name="phoneno" required placeholder="Contact number" class="form-control"><br>
+    </div>
  </div>   
-
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
  <div class="row">
-    <div class="col">
-    <input type="text" name="email" required placeholder="email" class="form-control"><br></div>
+    <div class="col-md-6">
+    <h5>Responsibilities </h5>
+    <p>* Design and follow a complete teaching plan</p>
+    <p>* Teach alphabet and numeracy along with personal, social and emotional skills</p>
+    <p>* Organize learning material and resources</p>
+    </div>
+    <div class="col-md-6" style="text-align:left;">
+    <input type="text" name="email" required placeholder="email" class="form-control"><br>
+    </div>
  </div> 
+ <br>
+
+ </select><br><br>
+ <div class="row">
+    <div class="col-md-6">
+    <h5>Requirements</h5>
+    <p>* Excellent knowledge of child development and latest education theories and practices</p>
+    <p>* Teaching and organisation skills</p>
+    <p>* Creative and artistic teaching abilities</p>
+    <p>* Patience and flexibility</p>
+    <p>* Strong communication skills</p>
+    </div>
+    <div class="col-md-6" style="text-align:left;">
+    <input type="submit" value="submit" name="submit" >
+    </div>
+</div>
+
+
+
 
 </form>
 
