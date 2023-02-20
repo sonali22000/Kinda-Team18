abb03c76055b694ac24f8a60a9fb68db4b7c0ea1
udayanga22000 <118618291+udayanga22000@users.noreply.github.com>
2023-02-20 22:20:47 +0200

    some changes

--- contact.php
+++ contact.php
@@ -26,17 +26,19 @@
                 <li><i class="material-icons" style="font-size:60px;color:blue;">email</i><a href="mailto:info@twinklingstars.com">info@twinklingstars.com</a></a></li><br>
                 
                 <li><i class="material-icons" style="font-size:60px;color:blue;">facebook</i><a href="https://www.facebook.com/profile.php?id=100089799731637">Like Us On Facebook</a></li>
+                <li><i class="material-icons" style="font-size:60px;color:blue;">Message Us</i>Message Us</li>
             </ul>
             </div>
+
+            <div class="col-md-6" style="color: aliceblue;">
+
+
           </div>
         
 
 <div class="row">
     <div class="col-md-6" style="color: aliceblue; text-align: center; font-size:35px; color: cornflowerblue; font-weight: bold; font-style: italic;  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;">
          
-      <p1>Get in touch<br>   
-        <br>     
-    </p1>
     </div>
     <div class="col-md-6">         
         </div>
@@ -46,25 +48,7 @@
     <div class="col-md-6" style="color: aliceblue; text-align: center; font-size: larger;">
  
     </div>
-    <div class="col-md-6" style="color: aliceblue;"> 
-
-        <form action="/action_page.php">
-            <label for="name" >Name*      : </label>
-            <div><input type="text" id="name" name="name" size="45"><br><br></div>
-            <label for="email">Email *    : </label>
-            <div><input type="text" id="email" name="email" size="45"><br><br></div>
-            <label for="subject">Subject : </label>
-            <div><input type="text" id="subject" name="subject" size="45"><br><br></div>
-            <label for="lname">Message* : </label>
-            <div><input type="text" id="msg" name="msg" size="45"><br><br></div>
-            <label for="lname">Phone *: </label>
-            <div><input type="text" id="phone" name="phone" size="45"><br><br></div>
-
-            <input type="submit" value="Submit" style="color:coral; font-weight: bold; background-color: blanchedalmond;">
-          </form>
-          
-        
-        </div>
+    
     </div>
 
                        
