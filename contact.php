80219f734464c35bef5f48d4a0b0f166021845dc
udayanga22000 <118618291+udayanga22000@users.noreply.github.com>
2023-02-21 06:33:32 +0200

    changes

--- contact.php
+++ contact.php
@@ -16,17 +16,18 @@
                 <li>Contact Us <br><br></li>
                 </div>
 
-                <div class="keepintouch" style="font-size: 30px;">
-                <li>   Keep In Touch</li><br>
+                <div class="keepintouch" style="font-size: 25px;">
+                <li><i>   Keep In Touch</i></li><br>
                 </div>
                 <li>Twinkling Stars Kindergarten  <br>Visamäentie 23,
-                <br>Hämeenlinna 13100</li><br><br>
+                <br>Hämeenlinna 13100</li><br>
                 <li><i class="material-icons" style="font-size:60px;color:blue;">phone</i>415-218-4791</li><br>
 
                 <li><i class="material-icons" style="font-size:60px;color:blue;">email</i><a href="mailto:info@twinklingstars.com">info@twinklingstars.com</a></a></li><br>
                 
-                <li><i class="material-icons" style="font-size:60px;color:blue;">facebook</i><a href="https://www.facebook.com/profile.php?id=100089799731637">Like Us On Facebook</a></li>
-                <li><i class="material-icons" style="font-size:60px;color:blue;">Message Us</i>Message Us</li>
+                <li><i class="material-icons" style="font-size:60px;color:blue;">facebook</i><a href="https://www.facebook.com/profile.php?id=100089799731637">Like Us On Facebook</a></li><br><br><br>
+                <li><i class="material-icons" style="font-size:80px;color:darkblue;">diversity_3</i>Our Team&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="material-icons" style="font-size:80px;color:darkblue;">sports_kabaddi</span>Our Kids</li>
+</span></li>
             </ul>
             </div>
 
@@ -51,6 +52,41 @@
     
     </div>
 
-                       
+  
+    
+    <div class="row">
+    <div class="col-md-6" style="color: aliceblue; text-align: center; font-size: larger;">
+              <img class="img-fluid" img src="Images/contactus.jpg" alt="Contact Us">
+    </div>
+    <div class="col-md-6" style="color: aliceblue;"> 
+
+    <div class="form-group mb-4" style="color: aliceblue;>
+              <label for="name">Your name*:</label>
+              <input id="name" type="text" name="name" class="form-control" placeholder="Example: Dhanushke Perera" style="width: 500px" required></input>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <label for="Email">E-mail address*:</label>
+              <input id="Email" type="email" name="email" class="form-control" placeholder="Example: xxxxxxxa@xxxxx.xxx" style="width: 500px" required>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <label for="subject">Subject:</label>
+              <input id="subject" type="text" name="subject" class="form-control" style="width: 500px" required>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <label for="subject">Phone Number*:</label>
+              <input id="subject" type="text" name="subject" class="form-control" style="width: 500px" required>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <label for="message">Message*:</label>
+              <textarea id="message" name="body" class="form-control" style="width: 500px" rows="4" placeholder="Your message here please..."  required></textarea>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <button type="submit" class="btn btn btn-yellow btn-outline" >
+              <input type="submit" value="Submit" style="color:coral; font-weight: bold; background-color: blanchedalmond;" class="btn btn btn-yellow btn-outline">
+            <div>
+        
+        </div>
+    </div>
+</div>
 
 <?php include "footer1.php" ?>
