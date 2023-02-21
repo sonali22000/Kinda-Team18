3f6a66317540f40ab56e8d31051a0dae698b3c41
udayanga22000 <118618291+udayanga22000@users.noreply.github.com>
2023-02-21 11:44:19 +0200

    db connect teachers

--- contact.php
+++ contact.php
@@ -26,7 +26,7 @@
                 <li><i class="material-icons" style="font-size:60px;color:blue;">email</i><a href="mailto:info@twinklingstars.com">info@twinklingstars.com</a></a></li><br>
                 
                 <li><i class="material-icons" style="font-size:60px;color:blue;">facebook</i><a href="https://www.facebook.com/profile.php?id=100089799731637">Like Us On Facebook</a></li><br><br><br>
-                <li><i class="material-icons" style="font-size:80px;color:darkblue;">diversity_3</i>Our Team&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="material-icons" style="font-size:80px;color:darkblue;">sports_kabaddi</span>Our Kids</li>
+                <li><a href="createteacersinfo.php"><i class="material-icons" style="font-size:80px;color:darkblue;">diversity_3</i>Our Team</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="createkidsinfo.php"><span class="material-icons" style="font-size:80px;color:darkblue;">sports_kabaddi</span>Our Kids</a></li>
 </span></li>
             </ul>
             </div>
@@ -39,86 +39,7 @@
 
 
 
-<div class="row">
-    <div class="col-md-6" style="color: aliceblue; text-align: center; font-size: larger;">
- 
-    </div>
-    
-    </div>
 
-  
-    
-    <div class="row">
-    
-              
-    <div class="form-group mb-4" style="color: aliceblue;>
-              <label for="teid">Teacher ID:</label>
-              <input id="teid" type="text" name="teid" class="form-control" placeholder="Example: 001" style="width: 500px" required></input>
-            </div>
-            <div class="form-group my-4" style="color: aliceblue;>
-              <label for="tname">Teacher Name:</label>
-              <input id="tname" type="text" name="tname" class="form-control" placeholder="Example: Adam Smith" style="width: 500px" required>
-            </div>
-            <div class="form-group my-4" style="color: aliceblue;>
-              <label for="phone">Phone Number:</label>
-              <input id="phone" type="number" name="phone" class="form-control" placeholder="Example: 01234567890" style="width: 500px" required>
-            </div>
-            <div class="form-group my-4" style="color: aliceblue;>
-              <label for="email">E-mail Address:</label>
-              <input id="email" type="email" name="email" class="form-control" placeholder="Example: xxxxxxxa@xxxxx.xxx" style="width: 500px" required>
-            </div>
-            <div class="form-group my-4" style="color: aliceblue;>
-              <label for="address">Address:</label>
-              <input id="address" type="address" name="address" class="form-control" placeholder="Example: Kaivoselantie 4, Hameenlinna"  style="width: 500px" required>
-            </div>
-            <div class="form-group my-4" style="color: aliceblue;>
-              <label for="bio">Bio:</label>
-              <textarea id="bio" name="body" class="form-control" style="width: 500px" rows="4" placeholder="Teacher's bio..."  required></textarea>
-            </div>
-            <div class="form-group my-4" style="color: aliceblue;>
-              <button type="view" class="btn btn btn-yellow btn-outline" >
-              <input type="view" value="View" style="color:coral; font-weight: bold; background-color: blanchedalmond;" class="btn btn btn-yellow btn-outline">
-            <div>
-        
-        </div>
-        
-        
-    </div>
-    </div>
-</div>
-<div class="row">
-    
-              
-    <div class="form-group mb-4" style="color: aliceblue;>
-              <label for="kidsid">Kids ID:</label>
-              <input id="kidsid" type="text" name="kidsid" class="form-control" placeholder="Example: 001" style="width: 500px" required></input>
-            </div>
-            <div class="form-group my-4" style="color: aliceblue;>
-              <label for="kidsname">Kids Name:</label>
-              <input id="kidsname" type="text" name="kidsname" class="form-control" placeholder="Example: Situ Anna" style="width: 500px" required>
-            </div>
-            <div class="form-group my-4" style="color: aliceblue;>
-              <label for="parentsname">Parents Name:</label>
-              <input id="parentsname" type="text" name="parentsname" class="form-control" placeholder="Example: Nura Pekka" style="width: 500px" required>
-            </div>
-            <div class="form-group my-4" style="color: aliceblue;>
-              <label for="dob">Kids Birth Day:</label>
-              <input id="dob" type="date" name="dob" class="form-control" placeholder="Example: 05.05.2020" style="width: 500px" required>
-            </div>
-            <div class="form-group my-4" style="color: aliceblue;>
-              <label for="phone">Contact Number:</label>
-              <input id="phone" type="number" name="phone" class="form-control" placeholder="Example: 01234567890"  style="width: 500px" required>
-            </div>
-            <div class="form-group my-4" style="color: aliceblue;>
-              <label for="address">Address:</label>
-              <input id="address" type="address" name="address" class="form-control" placeholder="Example: Kaivoselantie 4, Hameenlinna"  style="width: 500px" required>
-            </div>
-            <div class="form-group my-4" style="color: aliceblue;>
-              <button type="view" class="btn btn btn-yellow btn-outline" >
-              <input type="view" value="View" style="color:coral; font-weight: bold; background-color: blanchedalmond;" class="btn btn btn-yellow btn-outline">
-            <div>
-        
-        </div>
 
 
 <?php include "footer1.php" ?>
--- createteacersinfo.php
+++ createteacersinfo.php
@@ -4,61 +4,66 @@ include "headerr.php"?>
 
 <!-- creating form for teachersinfo-->
 <form  method="post"  action=""> 
- <div class="row">
-    <div class="col">
-    <input type="text" name="fname" required placeholder="Teachers First Name" class="form-control" ></div>
+<div class="row">
     
-    <div class="col">
-    <input type="text" name="lname" required placeholder="Teachers Last Name" class="form-control"></div>
+              
+    <div class="form-group mb-4" style="color: aliceblue;>
+              <label for="teid">Teacher ID:</label>
+              <input  type="text" name="teid" class="form-control" placeholder="Example: 001" style="width: 500px" required></input>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <label for="tname">Teacher Name:</label>
+              <input  type="text" name="tname" class="form-control" placeholder="Example: Adam Smith" style="width: 500px" required>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <label for="phone">Phone Number:</label>
+              <input  type="number" name="phone" class="form-control" placeholder="Example: 01234567890" style="width: 500px" required>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <label for="email">E-mail Address:</label>
+              <input  type="email" name="email" class="form-control" placeholder="Example: xxxxxxxa@xxxxx.xxx" style="width: 500px" required>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <label for="address">Address:</label>
+              <input  type="address" name="address" class="form-control" placeholder="Example: Kaivoselantie 4, Hameenlinna"  style="width: 500px" required>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <label for="bio">Bio:</label>
+              <textarea name="body" class="form-control" style="width: 500px" rows="4" placeholder="Teacher's bio..."  required></textarea>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <button type="submit" class="btn btn btn-yellow btn-outline" >
+              <input type="Submit" value="Submit" name="submit" style="color:coral; font-weight: bold; background-color: blanchedalmond;" class="btn btn btn-yellow btn-outline">
+            <div>
+        
+        </div>
+        
+        
+    </div>
 
-    <div class="col">
-    <input type="text" name="email" required placeholder="Teachers Email" class="form-control"> <br></div>
- </div> 
 
-
- <div class="row">
-   <div class="col">
-   <input type="text" name="phone" required placeholder="Phone Number" class="form-control" > </div>
-
-   <div class="col">
-   <input type="text" name="address" required placeholder="Address" class="form-control" ><br> </div>
-
-   <div class="col">
-   <input type="text" name="bio" required placeholder="Teachers Biography" class="form-control" > </div>
- </div>
-
-
- <div class="row"> 
-   <div class="col">
-   <input type="submit" value="Submit" name="Submit"></div>
- </div>
-</form>
-
-
-
-<?php
-//Getting values from end user
-if(isset($_POST['submit'])){
-    $fname=$_POST['fname'];
-    $lname=$_POST['lname'];
-    $email=$_POST['email'];
-    $phone=$_POST['phone'];
-    $address=$_POST['address'];
-    $bio=$_POST['bio'];
+    <?php
+if (isset($_POST ['submit'])){
+    $teid = $_POST['teid'];
+    $tname = $_POST['tname'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $bio = $_POST['bio'];
     include 'db.php';
-    $sql="insert into teachersinfo(fname,lname,email,phone,address,bio)
-    values('$fname','$lname','$email', '$phone', '$address', '$bio')";
-    if($conn->query($sql)===TRUE){
-                
-      echo"Your information is added successfully";
+    $sql = "insert into teachersinfo (teid,tname,phone,email,address,bio)
+    values('$teid','$tname','$phone','$email','$address','$bio')"; 
 
-  }
-  else{
+    if ($conn ->query($sql) === TRUE){
+        echo "Your information is added Successfully";
+    }
+    else {
+        echo "Error:" . $conn->error;
+    }
 
-      echo"Error:" .$conn->error;
-
-  }
 }
+
+
 ?>
 
 <?php include "footer1.php" ?>
