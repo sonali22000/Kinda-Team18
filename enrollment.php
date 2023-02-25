9f940e8cfae75da614bd3316c3948b99a9c73b8f
sonali22000 <116430498+sonali22000@users.noreply.github.com>
2023-02-25 18:44:36 +0200

    enrollment

--- enrollment.php
+++ enrollment.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php include "headerr.php" ?>
 <div class="row">
-<img class="header-image" src="https://th.bing.com/th/id/OIP.UeFNYlBO1xM03hoOFjU0mQAAAA?pid=ImgDet&rs=1 " alt="background image"class="rounded" width="500" height="400">>
+<img class="header-image" src="https://i.pinimg.com/originals/64/07/f8/6407f8b72e8c1afa198ddd37b81baef6.jpg  " alt="background image"class="rounded" width="500" height="400">>
 </div>  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
@@ -20,14 +20,27 @@
         <div class="col-md-12 text-center">
         <h6><b> info@twinklingstars.fi</b></h6>  
     </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-        <a class="btn btn-danger" href="page 5" role="button">Enrollment form</a>
-        </div>
+   
     
     
-</div>
+
+        
+                
+                
+                <li><a href="enrollmentform.php"><i class="material-icons" style="font-size:80px;color:black;">diversity_3</i>Enrollment Form</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="enrollmentform.php"><span class="material-icons" style="font-size:80px;color:black;">sports_kabaddi</span>FEES</a></li>
+</span></li>
+            </ul>
+            </div>
+
+            <div class="col-md-6" style="color: aliceblue;">
+
+
+          </div>
+        
+
+
+
+
 
 
 <?php include "footer1.php" ?>
---
+++ enrollmentform.php
@@ -0,0 +1,74 @@
+<?php
+$title="crup app for kindergarten";
+include "headerr.php"?>
+
+
+<!-- creating form for enrollment-->
+
+<form  method="post"  action=""> 
+<div class="row">
+    
+              
+    <div class="form-group mb-4" style="color: black;>
+              <label for="kidsname">Kids Name:</label>
+              <input name="kidsname" type="text" name="kidsname" class="form-control" placeholder="Example: Noah Fedrick" style="width: 500px" required></input>
+            </div>
+            <div class="form-group my-4" style="color: black;>
+              <label for="parentsdetails">Parents Name and Relationship:</label>
+              <input id="parentsdetails" type="text" name="parentsdetails" class="form-control" placeholder="Example: Elisbeth-Mother" style="width: 500px" required>
+            </div>
+            <div class="form-group my-4" style="color: black;>
+              <label for="email">email:</label>
+              <input id="email" type="int" name="email" class="form-control" placeholder="Example: abcde@gmail.com" style="width: 500px" required>
+            </div>
+            <div class="form-group my-4" style="color: black;>
+              <label for="Address">Address:</label>
+              <input id="Address" type="text" name="address" class="form-control" placeholder="Example: No 23 G 75 Helsinki" style="width: 500px" required>
+              </div>
+              <div class="form-group my-4" style="color: black;>
+              <label for="dob">Kids Birth Day:</label>
+              <input id="dob" type="date" name="dob" class="form-control" placeholder="Example: 05.05.2020" style="width: 500px" required>
+            </div>
+            <div class="form-group my-4" style="color: black;>
+              <label for="phone">Contact Number:</label>
+              <input id="phone" type="int" name="phone" class="form-control" placeholder="Example: 01234567890"  style="width: 500px" required>
+            </div>
+            <div class="form-group my-4" style="color: black;>
+              <button type="submit" class="btn btn btn-yellow btn-outline" >
+              <input type="Submit" value="Submit" name="submit" style="color:coral; font-weight: bold; background-color: blanchedalmond;" class="btn btn btn-yellow btn-outline">
+            <div>
+        
+        </div>
+
+
+
+
+
+<?php
+if (isset($_POST ['submit'])){
+    
+    $kidsname = $_POST['kidsname'];
+    $parentsnameandrelationship = $_POST['parentsnameandrelationship'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $dob = $_POST['dob'];
+    $phone = $_POST['phone'];
+   
+    
+    include 'db.php';
+    $sql = "insert into enrollmentform (kidsname,parentsnameandrelationship,email,address,dob,phone)
+    values('$kidsname','$parentsnameandrelationship','$email','$dob','$address','$dob','$phone')"; 
+
+    if ($conn ->query($sql) === TRUE){
+        echo "Your information is added Successfully";
+    }
+    else {
+        echo "Error:" . $conn->error;
+    }
+
+}
+
+
+?>
+
+<?php include "footer1.php" ?>
