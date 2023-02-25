48cd5439be46f3dbacb15b5bda0b6e20995e88c0
nedaMaghrebi <neda22000@student.hamk.fi>
2023-02-25 22:59:24 +0200

    update

--- kindergarten.js
+++ kindergarten.js
@@ -1,29 +1,13 @@
-//validateform for formprogram
+//valifdateform
 function validateForm(){
     let description=(document.formprogram.description.value).trim();
     if (description == "") {
-        alert("Please enter a description.");
+        alert("Please enter your description.");
         return false;
       }
-    }
-function validateForm(){
-    let capacity=(document.formprogram.capacity.value).trim();
-    if (capacity == "") {
-        alert("Please enter a valid capacity.");
-        return false;
-      }
-}
-function validateForm(){
-  let kidsid=(document.formprogram.kidsid.value).trim();
-  if (kidsid == "") {
-      alert("Please enter a valid kidsid.");
-      return false;
-    }
-}
-function validateForm(){
-  let teid=(document.formprogram.teid.value).trim();
-  if (teid == "") {
-      alert("Please enter a valid teid.");
-      return false;
-    }
+
+
+
+
+
 }
