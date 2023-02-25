13b92072f05e21466bca9959c08ea1ba4f0853d2
nedaMaghrebi <neda22000@student.hamk.fi>
2023-02-25 23:51:56 +0200

    some changes in js

--- kindergarten.js
+++ kindergarten.js
@@ -2,22 +2,22 @@
 function validateForm(){
     let description=(document.formprogram.description.value).trim();
     if (description == null ||description== "") {
-        alert("Description can't be blank.");
+        alert("Please enter a valid description.");
         return false;
       }
     let capacity=(document.formprogram.capacity.value).trim();
     if (capacity == null ||capacity== "") {
-        alert("capacity can't be blank.");
+        alert("Please enter a valid capacity.");
         return false;
       }
       let kidsid=(document.formprogram.kidsid.value).trim();
     if (kidsid == null ||kidsid== "") {
-        alert("kidsid can't be blank.");
+        alert("Please enter a valid kidsid.");
         return false;
         }
       let teid=(document.formprogram.teid.value).trim();
       if (teid == null ||teid== "") {
-        alert("teid can't be blank.");
+        alert(" Please enter a valid teid.");
         return false;
             }
 
