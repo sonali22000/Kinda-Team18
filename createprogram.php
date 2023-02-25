ce769eb04b56e07b41ee339518043926e7838a72
nedaMaghrebi <neda22000@student.hamk.fi>
2023-02-25 15:08:05 +0200

    add code to call kindergarten.js

--- createprogram.php
+++ createprogram.php
@@ -4,7 +4,6 @@
  <!-- creating different activities in kindergarten-->               
  <div class="row">
 
-
     <h4><b>A place where a child can play, explore and learn!</b> <br> <br></h4>
     <p>
     Preschool days are the most important phase for a child’s emotional development; It’s when the child builds his/her confidence, self esteem and 
@@ -53,15 +52,14 @@
 <form  method="post"  action=""> 
 
 
+
+
 <div class="row">
+
 <div class="col-md-6">
 <h4><b>Different types of program in our kindergarten</b> <br> <br></h4>
 <p>Our program table provides parents and guardians with a detailed schedule of daily activities, including the name of the activity, the time and duration of the activity, and the teacher or staff member responsible for the activity. We also include any special events or holidays that may affect the regular schedule.
-
-
 <br><br>We believe that parental involvement is key to the success of our program, and we encourage parents to stay informed and involved in their children's education and development. Our program table is just one of the many tools we use to keep parents informed and engaged in their children's learning journey.</p>
-
-
 </div>
 
    <div class="col-md-6">
@@ -89,8 +87,10 @@
 
   <input type="submit" value="AddProgram" name="AddProgram"> <br> <br> <br> <br> <br> <br>  </div>
 
-
 </div>
+
+
+
 </form>
 
 
--- headerr.php
+++ headerr.php
@@ -6,6 +6,7 @@
     <title>echo $title</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    
     <style>
 body {
     background-color: #F0D2D2;
@@ -175,12 +176,11 @@ a:active {
   text-decoration: underline;
 }
 </style>
+
+<script src="kindergarten.js"></script>
+
 </head>
-<!--<body>
-  <div class="container">
-    <header class="site_header">
-        <div class="wrapper"></div>
-        <nav>-->
+
         <body>
   <div class="container" >
     <div class="row">
