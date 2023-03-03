e99e18972da7d4e38d5cf364b120b0f5d03c5081
nedaMaghrebi <neda22000@student.hamk.fi>
2023-03-03 13:03:45 +0200

    add link for updating and deleting

--- readprogram.php
+++ readprogram.php
@@ -25,7 +25,7 @@ if($result->num_rows > 0){
         while($row = $result -> fetch_assoc()) {
             echo "
             <tr>
-            
+            <td><a href='updatedeletprogram.php?programid=$row[programid]'>$row[programid] </a>     </td>   
             <td> $row[programid]     </td>
             <td> $row[pname]     </td>
             <td> $row[description]      </td>
