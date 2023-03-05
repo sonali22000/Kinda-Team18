a5f257a08b2332c5b3a6f772251ed89d82097f65
sonali22000 <116430498+sonali22000@users.noreply.github.com>
2023-03-05 13:30:48 +0200

    done changes

--- enrollment.php
+++ enrollment.php
@@ -21,6 +21,8 @@
         <h6><b> info@twinklingstars.fi</b></h6>  
     </div>
 
+<br><br>
+
 
 <div class="row">
             <div class="col-md-6">
@@ -32,8 +34,7 @@
         
    
     
- <li><a href="enrollmentform.php"><i class="material-icons" style="font-size:60px;color:black text-center;">contacts</i>Enrollment Form</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="enrollmentform.php"></a></li>
-</span></li>
+ 
 
 
             </div>
@@ -43,6 +44,118 @@
 
           </div>
         
+<!-- creating form for program in enrollmentform-->
+
+<form  method="post"  action="" onsubmit="return validateForm()" name="enrol application"> 
+
+
+<div class="row">
+
+<div class="col-md-6">
+<h4><b>Why you should enroll your child to our kindergarten</b> <br> <br></h4>
+<p>Enrolling them in kindergarten is crucial for their learning and growth, and preparing them early will go a long way.
+<br><br>Educating your child privately is simply too large an investment to make without seeking professional advice.And we are ensure the best for your kid</p>
+
+
+<br><br><br><br><br><br><li><a href="enrollmentform.php"><i class="material-icons" style="font-size:60px;color:black text-center;">contacts</i>Enrollment Form</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="enrollmentform.php"></a></li>
+</span></li>
+
+
+
+</a>
+
+</div>
+
+   <div class="col-md-6">
+   <select name="pname">
+    <option value="ONLINE ADDMISSION"> ONLINE ADDMISSION </option>
+    <option value="ORDER BOOK LIST"> ORDER BOOK LIST </option>
+   
+    </select><br> <br>
+
+   
+   <input type="text" name="Name of the kid" required placeholder="Name of the kid" class="form-control"> <br>
+
+
+  <input type="text" name="Guardian Details" required placeholder="Please Enter Mother or Father's name" class="form-control" > <br>
+
+  <input type="date" name="Birth Date" required placeholder="Birth Date" class="form-control" > <br>
+
+
+  <input type="int" name="phone number" required placeholder="Phone Number" class="form-control" >  <br> 
+
+  <input type="int" name="KidsId" required placeholder="Kidsid" class="form-control" > <br> 
+
+  <input type="int" name="Age of Kid" required placeholder="Age of Kid" class="form-control" >  <br> 
+ 
+
+  <input type="submit" value="Apply" name="Apply"  <span onmouseover="this.style.color='red';
+                 this.style.backgroundColor='black';
+                 this.style.fontSize='1em'; ";
+      onmouseout="
+                 this.style.color='blue';
+                 this.style.backgroundColor='white';
+                 this.style.fontSize='1.1em';"; > 
+                 
+  
+
+
+  <br> <br> <br> <br> <br> <br>
+                
+                
+   </div>
+
+  
+
+</div>
+
+
+
+</form>
+
+
+
+<?php
+
+
+//Getting values from end user
+
+if(isset($_POST['Apply'])){
+   $pname=$_POST['pname'];
+   $description=$_POST['Name of the Kid'];
+   $startdate=$_POST['Please enter Mother or Fathers name'];
+   $enddate=$_POST['Birth Date'];
+   $capacity=$_POST['Phone Number'];
+   $kidsid=$_POST['kidsid'];
+   $teid=$_POST['Age of Kid'];
+   include 'db.php';
+   $sql="insert into program(pname,Name of the Kid,Please enter Mother or Fathers name,Birth Date,Phone Number,KidsId,Age of Kid)
+   values('$pname,$NameoftheKid,$PleaseEnterMotherorFathersname,$BirthDate,$PhoneNumber,$KidsId,$AgeofKid')";
+   if($conn->query($sql)===TRUE){
+               
+     echo"Your datas are added successfully";
+
+ }
+ else{
+
+     echo"Error:" .$conn->error;
+
+ }
+}
+
+?>
+       
+               
+
+
+
+<?php include "footer1.php" ?>
+
+
+
+
+
+
 
 
 
